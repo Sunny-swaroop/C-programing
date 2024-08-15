7c7e7e7155c3352657e30a2a79b12893ae6a94fc
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-15 09:54:02 +0000

    basic examples

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	a=10;
+	b=10;
+	c=a+b;
+	printf("the sum of %d and %d is %d",a,b,c);
+	return 0;
+}
+
---
+++ subtraction.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,result;
+	a=10;
+	b=5;
+	result=a-b;
+	printf("the subtraction of %d and %d is %d",a,b,result);
+	return 0;
+}
+
