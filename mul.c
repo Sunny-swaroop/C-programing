fe88d9932cf3a1230695a971031a178557511bbe
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-15 14:15:50 +0000

    multiplication using scanf function

---
+++ mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of first number",a);
+	scanf("%d",&a);
+	printf("enter the value of second number",b);
+	scanf("%d",&b);
+	c=a*b;
+	printf("the multiplication of %d and %d is %d",a,b,c);
+	return 0;
+}
+
