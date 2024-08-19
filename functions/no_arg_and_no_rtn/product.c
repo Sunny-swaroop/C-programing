df7aabd65853db1fff50281ef4738904316d67ce
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-19 09:29:34 +0000

    examples

--- functions/no_arg_and_no_rtn/product.c
+++ functions/no_arg_and_no_rtn/product.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
-//no arguments and no return type example
-//sum of two numbers
-void sum()
+void product()
 {
 	int num1,num2,result;
 	printf("enter the first number:",num1);
 	scanf("%d",&num1);
 	printf("enter the second number:",num2);
 	scanf("%d",&num2);
-	result=num1+num2;
-	printf("the sum of %d + %d =%d\n",num1,num2,result);
+	result=num1*num2;
+	printf("the product of %d * %d = %d\n",num1,num2,result);
 }
 int main()
 {
-	sum();
+	product();
 	return 0;
-
 }
+
