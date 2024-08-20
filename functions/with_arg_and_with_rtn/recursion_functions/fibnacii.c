3466fd1fc51592a35587832e16bf3074fdc988fc
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-20 11:57:09 +0000

    fibinancii series

---
+++ functions/with_arg_and_with_rtn/recursion_functions/fibnacii.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int fib(int num)
+{
+	if (num==0)
+	{
+		return 0;
+	}
+	else if (num==1)
+	{
+		return 1;
+	}
+	else
+	{
+		return(fib(num-1)+fib(num-2));
+	}
+}
+
+// Function to print the Fibonacci series
+int main()
+{
+	int num1;
+	printf("enter a value to know its fibinacci series:",num1);
+	scanf("%d",&num1);
+	for (int i=0;i<num1;i++)
+	{
+		printf("%d\n",fib(i));
+	}
+	return 0;
+}
+
