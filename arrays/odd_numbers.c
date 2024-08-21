a18f5f6fcc2970df91cf8ab222138a4418448d02
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-21 13:40:06 +0000

    odd numbers display

---
+++ arrays/odd_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,num,result;
+	printf("the array size is 100 enter the value upto 100\n");
+	printf("enter the size of variables:",num);
+	scanf("%d",&num);
+	result=0;
+	for(i=1;i<=num;i++)
+	{
+		if(i%2==1)
+		{
+			a[result]=i;
+			result++;
+		}
+	}
+	for(i=0;i<result;i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
+
