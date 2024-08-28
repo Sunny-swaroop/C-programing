8b3b25bafccaaf855e88aa7fc44e835fa5e30fde
EC2 Default User <ec2-user@ip-172-31-45-21.ec2.internal>
2024-08-28 14:43:22 +0000

    enum functions

---
+++ enum/weekdays.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// Define an enum for days of the week
+enum Weekday {
+    Sunday,     // 0
+    Monday,     // 1
+    Tuesday,    // 2
+    Wednesday,  // 3
+    Thursday,   // 4
+    Friday,     // 5
+    Saturday    // 6
+};
+
+int main() {
+    /* Declare a variable of type enum Weekday
+    enum Weekday today;
+
+    // Assign a value to the enum variable
+    today = Wednesday;
+
+    // Print the value stored in the enum
+    printf("Today is day number: %d\n", today);
+    */
+	int n;
+	printf("enter the week number:",n);
+        
+    return 0;
+}
+
